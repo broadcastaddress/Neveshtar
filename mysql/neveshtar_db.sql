f249ac674f69407ba8e11d43b6803a2f2e620067
Hossein Jabbari <mr.hhkj@gmail.com>
2015-04-03 17:11:39 +0430

    MySQL File Updated for missing migrations

--- mysql/neveshtar_db.sql
+++ mysql/neveshtar_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2015 at 11:30 AM
+-- Generation Time: Apr 03, 2015 at 12:40 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.14
 
@@ -62,6 +62,91 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories_media`
+--
+
+CREATE TABLE IF NOT EXISTS `categories_media` (
+`id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+`id` int(10) unsigned NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `item_id` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` int(1) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+`id` int(10) unsigned NOT NULL,
+  `title` varchar(225) COLLATE utf8_unicode_ci NOT NULL,
+  `subtitle` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `intro` longtext COLLATE utf8_unicode_ci,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `quote` text COLLATE utf8_unicode_ci,
+  `quote_author` text COLLATE utf8_unicode_ci,
+  `image_id` int(11) DEFAULT NULL,
+  `video_id` int(11) DEFAULT NULL,
+  `slug` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `language` varchar(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'fa',
+  `status` int(1) NOT NULL DEFAULT '0',
+  `access_level` int(2) NOT NULL DEFAULT '1',
+  `category_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_media`
+--
+
+CREATE TABLE IF NOT EXISTS `item_media` (
+`id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_tag`
+--
+
+CREATE TABLE IF NOT EXISTS `item_tag` (
+`id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `languages`
 --
 
@@ -85,6 +170,22 @@ INSERT INTO `languages` (`id`, `name`, `created_at`, `updated_at`, `language`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `media`
+--
+
+CREATE TABLE IF NOT EXISTS `media` (
+`id` int(11) NOT NULL,
+  `type` varchar(3) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'img',
+  `url` varchar(125) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(225) COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -92,16 +193,6 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_persian_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`migration`, `batch`) VALUES
-('2014_10_12_000000_create_users_table', 1),
-('2014_10_12_100000_create_password_resets_table', 1),
-('2015_02_01_005809_create_session_table', 2),
-('2015_02_01_005938_create_cache_table', 3);
 
 -- --------------------------------------------------------
 
@@ -132,8 +223,21 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `payload`, `last_activity`) VALUES
-('35ee8e08d11b80c95bb59ae639d41728b6ffc235', 'ZXlKcGRpSTZJa2hXUzNkS2JtVllWR0ZxY0dOU2NsWlBXSEJ2VlVFOVBTSXNJblpoYkhWbElqb2lhM0pMU1U5cE5rNWxXVE5GUmxwS2RERTNka3hTUWtwUWJGQmFNbU5GT0c1dWRub3hSMVIyT1Vsdk5FMXhPVlJ2TVhscmFGSndLM3BNVmsxWGJEbG1hRm93V21KeE5XcGNMMXB4VWxVM2JrUTVUM05LWlZGTWVYaDNSMHd4WkRoaGNFRnpjMnRSUzIwMlEzbHVLM1JrVTF3dk0ydFVLMWxDYlVGdGNHSnRXVGxaYm14TWJGTmFTR1JCT0d0M2RIZEVaelp0VUZsbVdVTmhVbFJ6WEM4d2RXeDBkamt5Y21wTGNXTlFVVGhSWlZORVF6SkROMGhyTjA5UFRWRTVXVFJpVlV0blFrdHhhbTVCWlVSNFl6QnpVRmx4U2tkb1NqRlVWMmczTXpsSFN6SmtLMHBVYlVoM05rNHlXbWxMTlZoNGNrZHNibFphVDFsWE5GTnlZbUY1VEVaRGFtVk5NSEo1VVd0VVRFMUZkRlJUY0hwU1NtWkRjWGd6UlRWRGVrMVpRbG94YzFaWFlYRkhNMUZEUTFsdVVYTkhNbE55V1hoSVozWTNUMlJ1VFdkSlpFNDRUVkJ2UjBaVmMwMURURU5pZW00eWVYaDRPR0pMZVRSSFZHWnBUV1I1VUZGRFUxd3ZhbVY1TVRNMFVpdGxjMVJvWlV4VlhDOVpiRmhpYVdaR2JXWmxOVFVpTENKdFlXTWlPaUl4Wm1SaVkyWmlZVEpqTjJNd1pESmtPRFE1TW1NMllqbGxOR1kzWkRaaFpqZzVPVFJsTXpNd00yRXlaRFV5TnpkaU5HVTNNelZrWVRka1lUTm1PREF5SW4wPQ==', 1426941057),
-('9d73b7b66f21af97ddc69497eb9d106490d06793', 'ZXlKcGRpSTZJbXBjTHpkTmNrTm5PRE14VG5ZclMwSXlhR2RhWkZoUlBUMGlMQ0oyWVd4MVpTSTZJbWwwV0V4RldrSmtOSHBYYVc1bE1YazJNekJIWVhVeFluRjVZek5GVUdSTFFVTk9kMUE0WmxZMVYydG5Za2RFZEhka2NXVXJhV0ZQUWxGUmQycDFLMUJ1TkZCQ1ExcFFRamR2V25aRVdsRTVXbmhGUmpOM1Nsd3ZOVEF4WWpsMFZHaHJaMGdyVFZoR2VHRnBjVzgzWWpreGJUbHJOV042UkZ3dmVpdFpjbFZUYzAxeGQxQmFSV1JFWXpaY0wybDFWamgwVGpFMWRrc3hNbWQ0ZEdsSVRsWmtWVmxGVWxsU2VHWTRTbk5JWkdOaU9WQm9kWEZVYW1SbVUweHRjazFJSzFkVVZHaDZVSFZxTWpGR2IyOWNMMUZGU2s5cmRVWmllV04yWm5CclkzSkNjSGhQUTFCMWNGUjFhMHB4V1ZOU1Mxd3ZWbWxMVEVWS2RVbExOMjR5YTJkWU5sSktOazAwZEVSSU5FUlBSbmM0WjNOUVEyVm5NR2xKTURGWE9FUkJORkZuTWtReVIyazJOMmRSTjBOMmJERnFXbW8zYW1SV2FqRm1YQzlFYlZwYVJ6ZFVNakpHWWpCeVRqWTNRbnAwWjNKb05VdElOVFpWY2s5dVYzVlFabTVRUWtKd01WVm5iVE50YjFKNFdFTllZV2RKVFZrM00ycEJSMnAwSzNSYVNXRlhjbTVZYVVaTWIwUk5TRmN5T0ZsTUswZEZWVEFyYldaS05GRTlQU0lzSW0xaFl5STZJalUyT0RSaVpXUTVaVGN6TXprMU9ERm1ZV0psTXpZeFpqUmpabVkwT1RJeE1qZ3dZV0ZtTXpSaE5qRmpORE5qTVdOak9EWmpPV0ZqTlROa05qRXpNemNpZlE9PQ==', 1426948306);
+('313d38a6e94458d997ffc891d84186436903932e', 'ZXlKcGRpSTZJa05uWkd0Wk5HZzNSazFHUkhaNWNGWlBiVFZYWVhjOVBTSXNJblpoYkhWbElqb2lPRW95WEM5RWVrcFJWMDFCYVVGM1VIVkhNMXd2YVhjeWNIRnRhV1JpU1ZkTE9WSXdhVEIyVm1SM1kzVmtaamRuT1UxRWFsd3ZjWGxOUWxoMlRVNDNjM0IxUTJ0QlNVRXlZM2htZGpWY0wwOVZORnd2WVRWMWEzbHRVREpZVWxZd1hDOXdURzRyWkVkdFFrNHhiVTF2VkZ3dlZYcGxkM1Z3V0dweFltaFVUMU5PWWpCNWJsVTNWamhEZDFSU1JtazVlbTQ0YVV3eGRHczNUR013TlVVeU5sUmFVRk5LU25NM2RYRTFLMGhCUW1OTlJXSmhTMUU1YXpaMGFGRnFSamhFYUVSUlpsUlVSMXd2U21SY0wweEdNbWxvVUVORWNrbFJkM2hIY2xCd1JrTTJablJGTUcxdVoycExNbXBsVFRsMVJUWmlTV0pXZG5oa2RtNU1TMUZzU1dwa2MxZHNXREZqTnpkbGVsSk5ORmg2WTFwMFJGWTNVMVF6Ym1RM2JtVjNWRFZEY1hoSWVsRk5PRGRxVURScWQyeDRVa1F4VEcxcWNuWlVSSGxTYUhSeWF6WkxTMlpUVVU0MVltUllWbEZIYTJzcmJ6WXdjQ3R4VFVveGMwdE5TemxhUlZoSlVWSktWbmR0ZGpFMmJETnROR1ZrWjNSbVRVdG1ORFZDWjBwQ2FVWjNRVGQzWEM5NFFVMXZSWG95WWxSRU1FbFhORlpsUlhwUlVVaENOMlIwWjNBMk9GZ3hTSFJSYW00d05HRktSVGxXZFdOMlJXOUVNV3RVU0U1cmJXUkVjR2xPU0VKMWJVb2lMQ0p0WVdNaU9pSXhPVGMzTXpWa09USXpOMll3WkRjeU56RmtNVFk1TTJJeE1UY3hZekE1TWpBM1pEVmlPVEpsWkdRMk5tVTFOMkprWWpNME9HWmtObU5oWmpFMk5URmpJbjA9', 1428064712);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+`id` int(10) unsigned NOT NULL,
+  `tag` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -156,7 +260,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Hossein Jabbari', 'mr.hhkj@gmail.com', '$2y$10$snzoiPWn.PqZKOKIa99OlOju.4sPlS.kdTA0fvO6h.HowBh6xPSG.', 'zrUvLpo9ZucIBkPO9fMwTCrXDg1RBjcvSunuN7s9woTvABVSdt8FQaYrC8Zx', '2015-01-31 21:19:07', '2015-01-31 21:19:07');
+(1, 'Hossein Jabbari', 'mr.hhkj@gmail.com', '$2y$10$snzoiPWn.PqZKOKIa99OlOju.4sPlS.kdTA0fvO6h.HowBh6xPSG.', 'ekYk0ZqiCpcOWd0wDhEtHViZJHKWjuD1NwpYdvizRxjTlZgO2xt85vu7CVa1', '2015-01-31 21:19:07', '2015-01-31 21:19:07');
 
 --
 -- Indexes for dumped tables
@@ -175,10 +279,46 @@ ALTER TABLE `categories`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `categories_media`
+--
+ALTER TABLE `categories_media`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `item_media`
+--
+ALTER TABLE `item_media`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `item_tag`
+--
+ALTER TABLE `item_tag`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
 -- Indexes for table `languages`
 --
 ALTER TABLE `languages`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `language` (`language`);
+
+--
+-- Indexes for table `media`
+--
+ALTER TABLE `media`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `password_resets`
@@ -191,6 +331,12 @@ ALTER TABLE `password_resets`
 --
 ALTER TABLE `sessions`
  ADD UNIQUE KEY `sessions_id_unique` (`id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -208,10 +354,45 @@ ALTER TABLE `users`
 ALTER TABLE `categories`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `categories_media`
+--
+ALTER TABLE `categories_media`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `item_media`
+--
+ALTER TABLE `item_media`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `item_tag`
+--
+ALTER TABLE `item_tag`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `languages`
 --
 ALTER TABLE `languages`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `media`
+--
+ALTER TABLE `media`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
